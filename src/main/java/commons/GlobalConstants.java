9728952d4326d3511933b513fc2ed8bb5602b354
Le Duc Tho <leductho911@gmail.com>
2023-08-25 17:01:55 +0700

    Setting config for multiple environment. Now you can run the test with parameter Denvironment=enviromentName. Example mvn clean test -Denvironment=dev

--- src/main/java/commons/GlobalConstants.java
+++ src/main/java/commons/GlobalConstants.java
@@ -1,8 +1,23 @@
 package commons;
 
+import org.aeonbits.owner.ConfigFactory;
+import utils.Environment;
+
 import java.io.File;
 
 public class GlobalConstants {
+	static Environment environment;
+
+	static {
+		String environmentName = System.getProperty("environment");
+		if (environmentName != null) {
+			ConfigFactory.setProperty("env", environmentName);
+		} else {
+			throw new IllegalStateException("'environment' system property is not set.");
+		}
+		environment = ConfigFactory.create(Environment.class);
+	}
+
 
 	public static final String PROJECT_PATH = System.getProperty("user.dir");
 	public static final String OS_NAME = System.getProperty("os.name");
@@ -11,7 +26,7 @@ public class GlobalConstants {
 	public static final long LONG_TIMEOUT = 30;
 
 	public static final String UPLOAD_FILE = PROJECT_PATH + File.separator + "uploadFiles" + File.separator;
-	public static final String HOMEPAGE_URL = "https://demo.nopcommerce.com/";
+	public static final String HOMEPAGE_URL = environment.appUrl();
 	public static final String LOGIN_URL = HOMEPAGE_URL + "login?returnUrl=%2F";
 	public static final String REGISTER_URL = HOMEPAGE_URL + "register?returnUrl=%2F";
 
